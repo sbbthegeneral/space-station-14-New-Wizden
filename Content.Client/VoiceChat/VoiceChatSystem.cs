c5e9d216772687005b0e751ac388d379e959dd47
Steven <sbbthegeneral@gmail.com>
2026-06-24 23:22:13 +1000

    Added OpenTK.Audio.OpenAL to Content.Client and created the Voice Chat system file there too

---
+++ Content.Client/VoiceChat/VoiceChatSystem.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK.Audio.OpenAL;
+
+namespace Content.Client.VoiceChat;
+
+internal class VoiceChatSystem
+{
+    ALCaptureDevice PlaceHolder;
+}
